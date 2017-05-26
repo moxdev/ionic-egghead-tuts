d34f5d67dd920df63d4f049f1c1b10b301d9ce0a
moxdev <moxdev1@gmail.com>
2017-05-26 10:49:16 -0400

    Lazy Loading Data

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,13 +8,36 @@ import { PeopleProvider } from '../../providers/people/people';
 })
 export class HomePage {
 
-  public people = this.service.getPeople();
+  public people = [];
   public shouldReorder = false;
 
   constructor(
     public navCtrl: NavController,
     public service: PeopleProvider
-  ) {}
+  ) {
+    this.service.getPeople()
+    .subscribe(
+      data => this.people = data.results
+    )
+  }
+
+  doInfinite(e){
+    this.service.getPeople()
+    .subscribe(
+      data => this.people.push(...data.results),
+        err => console.log(err),
+        () => e.complete()
+    )
+  }
+
+  doRefresh(e){
+    this.service.getPeople()
+    .subscribe(
+      data => this.people.unshift(...data.results),
+        err => console.log(err),
+        () => e.complete()
+    )
+  }
 
   toggleReorder() {
     this.shouldReorder = !this.shouldReorder
